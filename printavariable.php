ba89cf9f3a8c73d688bd1f0ffaa56c6234cff330
boomerman <Tarunpreet96@gmail.com>
2016-09-26 11:14:41 +0530

    text via variable

---
+++ printavariable.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>
+mera php page
+</title>
+</head>
+<body>
+<p style="font-size:40px; color:blue;">today we are going to print a variable usign php...</p>
+<?php
+$var = "hi! i am printed via variable!!!";
+echo $var;
+?>
+</body>
+</html>
