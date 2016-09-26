bb7d15f656183827b1116f38350b72b04fda0bb5
boomerman <Tarunpreet96@gmail.com>
2016-09-26 11:23:11 +0530

    !important print an integer

--- printavariable.php
+++ printavariable.php
@@ -7,8 +7,13 @@ mera php page
 <body>
 <p style="font-size:40px; color:blue;">today we are going to print a variable usign php...</p>
 <?php
-$var = "hi! i am printed via variable!!!";
-echo $var;
+$var = 5;
+$var1=4;
+$var3=$var + $var1;
+echo "a= $var<br> b=$var1 <br>";
+echo "c= $var3";
+
+
 ?>
 </body>
 </html>
